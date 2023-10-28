93c721714fd253e70d21040a88744092c5524ccc
Dylan-T-Peterson <dylantypeterson@gmail.com>
2023-10-27 23:47:04 -0800

    created req file and found/formatted census data

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 data sources:
 https://www.quorum.us/data-driven-insights/age-of-congress/
-https://bioguide.congress.gov/search?index=%22bioguideprofiles%22&size=12&matches=[]&filters={%22jobPositions.congressAffiliation.congress.name%22:[%22The%20118th%20United%20States%20Congress%22]}&sort=[{%22_score%22:true},{%22field%22:%22unaccentedFamilyName%22,%22order%22:%22asc%22},{%22field%22:%22unaccentedGivenName%22,%22order%22:%22asc%22},{%22field%22:%22unaccentedMiddleName%22,%22order%22:%22asc%22}]
+https://www2.census.gov/programs-surveys/demo/tables/age-and-sex/2022/age-sex-composition/2022agesex_table1.xlsx
--- proj.py
+++ proj.py
@@ -2,12 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import fastparquet
 
 
-def mass_astype(df: pd.DataFrame, series: list[str], dtype: str):
+def mass_astype(df: pd.DataFrame, series: list[str] | pd.Index, dtype: str):
     for x in series:
         df[x] = df[x].astype(dtype)
+
+
+def roundup(num: int, up: int) -> int:
+    return (num) - (num % up) + up
 
 
 def Quorum_to_df():
@@ -34,37 +37,100 @@ def Quorum_to_df():
         )
     )
 
-    df.loc[df["Title"] != df["title"], "Title"] = df["Title"].str.cat(
-        df["title"], sep=" - "
-    )
+    df.loc[df.Title != df.title, "Title"] = df.Title.str.cat(df.title, sep=" - ")
     del df["title"]
 
-    df["State"] = df["Name"].str.rsplit(n=1, expand=True)[1].str.strip("()")
+    df["State"] = df.Name.str.rsplit(n=1, expand=True)[1].str.strip("()")
     df.State = df.State.str.split(pat="-", n=1, expand=True)[1]
 
     df["District"] = df.State.str.rsplit(pat="-", n=1, expand=True)[1]
     df.State = df.State.str.split(pat="-", n=1, expand=True)[0]
 
-    df.Name = df["Name"].str.split(n=1, expand=True)[1]
-    df.Name = df["Name"].str.rsplit(n=1, expand=True)[0]
+    df.Name = df.Name.str.split(n=1, expand=True)[1]
+    df.Name = df.Name.str.rsplit(n=1, expand=True)[0]
 
     mass_astype(df, ["Name", "Birth"], "string")
     mass_astype(df, ["Age", "Terms"], "Int8")
-    # df = df.astype(
-    #     {"Name": "string", "Birth": "string", "Age": "Int8", "Terms": "Int8"}
-    # )
-    print(df.dtypes)
-    # Sanatize and type csv into proper df
+    df.to_parquet(
+        "data/118th-congress-ages.snappy.parq",
+        engine="fastparquet",
+        compression="snappy",
+    )
+    # df.to_excel("data/118th-congress-ages.xlsx") # needs openpyxl
 
 
-def csv_merge():
-    file1, file2 = "data/age-of-118th-congress.csv", "data/search.csv"
-    pass
+def Census_to_df():
+    df = pd.read_csv("data/2022agesex-census.csv")
+    df.Age = df.Age.astype("string")
+    mass_astype(df, df.columns[1:], dtype="float32")
+    df.loc[0:26, ["Bnumber", "Mnumber", "Fnumber"]] = df.loc[
+        0:26, ["Bnumber", "Mnumber", "Fnumber"]
+    ].mul(1000)
+    df.to_parquet(
+        "data/2022agesex-census.snappy.parq", engine="fastparquet", compression="snappy"
+    )
 
 
 def main():
-    pass
+    congress = pd.read_parquet("data/118th-congress-ages.snappy.parq")
+    census = pd.read_parquet("data/2022agesex-census.snappy.parq")
+    fig, (ax1, ax2) = plt.subplots(nrows=2)
+
+    # congress.Age.value_counts().sort_index().plot(kind="bar", ax=ax1)
+    def congress_graph(ax):
+        bins = pd.cut(x=congress.Age, bins=range(25, 91, 5), right=False)
+        sns.barplot(data=bins.value_counts(normalize=True) * 100, ax=ax)
+
+        labels = [
+            str(x) for x in pd.interval_range(start=25, end=85, freq=5, closed="left")
+        ]
+        labels.append("85+")
+        ax.set_xticks(ticks=np.arange(13), labels=labels)
+        ax.set_xlabel("Age of Congress")
+
+    def uspop_0plus(ax):
+        sns.barplot(
+            x=range(0, 86, 5),
+            y=census.Bpercent.iloc[1:19],
+            ax=ax,
+            # native_scale=True,
+        )
+
+        labels = [
+            str(x) for x in pd.interval_range(start=0, end=85, freq=5, closed="left")
+        ]
+        labels.append("85+")
+        ax.set_xticks(
+            ticks=range(18),
+            labels=labels,
+        )
+        ax.set_xlabel("Age of US Population")
+
+    def uspop_25plus(ax):
+        sns.barplot(
+            x=range(25, 86, 5),
+            y=census.Bpercent.iloc[6:19],
+            ax=ax,
+        )
+
+        labels = [
+            str(x) for x in pd.interval_range(start=25, end=85, freq=5, closed="left")
+        ]
+        labels.append("85+")
+        ax.set_xticks(
+            ticks=range(13),
+            labels=labels,
+        )
+        ax.set_xlabel("Age of US Population")
+
+    congress_graph(ax1)
+    uspop_25plus(ax2)
+    # print(census.iloc[6:19])
+    plt.show()
+
+    # print(congress.loc[df.Age.isna(), "Name"]) # Find birthdays for these Congress members
+    # use fig.canvas.mpl to create eventhandler for button press to swap between uspop 0 plus and 25 plus
 
 
 if __name__ == "__main__":
-    Quorum_to_df()
+    main()
